cae9b33d9ef14b7628e52c6a030080ea000fef10
pftfactory <developer@pftfactory.deca.jp>
2025-11-15 17:22:57 +0900

    Initial Commit

---
+++ MyohoTests/MyohoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyohoTests.swift
+//  MyohoTests
+//
+//  Created by pftFactory on 2025/11/15.
+//
+
+import Testing
+@testable import Myoho
+
+struct MyohoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
